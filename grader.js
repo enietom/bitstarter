c01ff8a07d49d72d61e15eeb3b5c61063a9cc1ab
Eduardo Nieto <enietom@gmail.com>
2013-07-12 07:07:52 +0000

    Final version for HW3

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_TEMP_FILE = ".index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -64,11 +66,28 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url>', 'URL to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    var outJson; 
+    if (program.file) {
+        checkJson = checkHtmlFile(finalFile, program.checks);
+        outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    } else if (program.url) {
+        rest.get(program.url).on('complete', function(result) {
+        if (result instanceof Error) {
+          console.log('Error: ' + result.message);
+        } else {
+          console.log('Escribiendo archivo ' + URL_TEMP_FILE);
+          fs.writeFileSync(URL_TEMP_FILE, result);
+          checkJson = checkHtmlFile(URL_TEMP_FILE, program.checks);
+          outJson = JSON.stringify(checkJson, null, 4);
+          console.log(outJson);
+        }
+      });
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
